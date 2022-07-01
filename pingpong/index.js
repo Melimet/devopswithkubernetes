5d600244b43f3d2e9fa26c014b2530e7407e6fdc
Melimet <erkka.rahikainen@gmail.com>
2022-07-01 20:59:22 +0300

    1.11

--- pingpong/index.js
+++ pingpong/index.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 const config = require('./utils/config')
 const http = require('http')
+const fs = require('fs')
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+const filePath = path.join(directory, 'text.txt')
 
 const server = http.createServer(app)
 
 var counter = 0
 app.get('/pingpong', (req, res) => {
     counter++
-    res.send('<p>pong ' + counter + "</p>")
+    const output = "pong " + counter
+    const output2 = '<p>pong ' + counter + "</p>"
+    output.data.pipe(fs.createWriteStream(filePath))
+    res.send(output2)
 })
 
 server.listen(config.PORT, () => {
